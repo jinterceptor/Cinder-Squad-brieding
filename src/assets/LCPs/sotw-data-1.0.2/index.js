aa1ed69de62efd9bd70de7e89aaea16e110deb5d
jinterceptor <jensen.cocker@gmail.com>
2025-06-07 19:52:05 +0100

    Create index.js

---
+++ src/assets/LCPs/sotw-data-1.0.2/index.js
@@ -0,0 +1,12 @@
+"use strict"
+import b from "./bonds.json"
+import f from "./frames.json"
+import s from "./systems.json"
+
+const data = {
+  bonds: b,
+  frames: f,
+  systems: s,
+}
+
+export default data;
