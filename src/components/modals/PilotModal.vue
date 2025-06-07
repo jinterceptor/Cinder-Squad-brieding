40a8259cafc06db1a279ef8572b0e6897bfc2cc1
jinterceptor <jensen.cocker@gmail.com>
2025-06-07 15:29:07 +0100

    Update PilotModal.vue

--- src/components/modals/PilotModal.vue
+++ src/components/modals/PilotModal.vue
@@ -20,45 +20,55 @@
 			<div class="col"><img src="/faction-logos/msmc.svg"></div>
 		</div>
 		<div class="body">
-			<div class="add-padding"> Union Administrative RM-4 Pilot Identification Protocol (IDENT) Record
-				{{ pilot.id }} </div>
+			<div class="add-padding"> Union Administrative RM-4 Pilot Identification Protocol (IDENT) Record {{ pilot.id }} </div>
 			<div class="flex-container-rows">
 				<div class="row add-padding">
 					{{ reverse(pilot.name) }}:{{ pilot.id }}//NDL-C-BLIND-REACH
 				</div>
 				<div class="row flex-container-cols add-padding">
 					<div class="col grow-max flex-container-rows" style="padding-top:5px">
-						<div class="row flex-container-cols" >
-							<div class="col col-primary"><span class="flavor-text"> Callsign: <b class="accent--text">{{
-								capitalize(pilot.callsign) }}</b><br> Name (or legal alias): <b
-										class="accent--text">{{ pilot.name }}</b><br> Background: <b
-										class="accent--text"> {{ pilot.background }} </b></span></div>
-							<div class="col">CALLSIGN AVAILABLE <br> IDENTITY
-								VERIFIED <br> PH/HR DATA REGISTERED</div>
+						<div class="row flex-container-cols">
+							<div class="col col-primary">
+								<span class="flavor-text">
+									Callsign: <b class="accent--text">{{ capitalize(pilot.callsign) }}</b><br>
+									Name (or legal alias): <b class="accent--text">{{ pilot.name }}</b><br>
+									Background: <b class="accent--text"> {{ pilot.background }} </b>
+								</span>
+							</div>
+							<div class="col">CALLSIGN AVAILABLE <br> IDENTITY VERIFIED <br> PH/HR DATA REGISTERED</div>
 						</div>
-						<div style="padding-top:5px"> FRAME CONFIGURATION OPTIONS <span class="subtle--text">("H.A.S.E"
-								OMNINET VAULT REMIT)</span></div>
-						<div class="row" style="padding-top:5px"><span
-								style="font-size: 22px; line-height: 15px;"> [
-								HULL: <span class="stat-text accent--text" style="font-size: 24px;"> {{ pilot.mechSkills[0] }} </span> 
-								AGI: <span class="stat-text accent--text" style="font-size: 24px;"> {{ pilot.mechSkills[1] }} </span> 
-								SYS: <span class="stat-text accent--text" style="font-size: 24px;"> {{ pilot.mechSkills[2] }} </span> 
-								ENG: <span class="stat-text accent--text" style="font-size: 24px;"> {{ pilot.mechSkills[3] }} </span> ] </span></div>
+						<div style="padding-top:5px">FRAME CONFIGURATION OPTIONS
+							<span class="subtle--text">("H.A.S.E" OMNINET VAULT REMIT)</span>
+						</div>
+						<div class="row" style="padding-top:5px">
+							<span style="font-size: 22px; line-height: 15px;">
+								[
+								HULL: <span class="stat-text accent--text" style="font-size: 24px;">{{ pilot.mechSkills[0] }}</span>
+								AGI: <span class="stat-text accent--text" style="font-size: 24px;">{{ pilot.mechSkills[1] }}</span>
+								SYS: <span class="stat-text accent--text" style="font-size: 24px;">{{ pilot.mechSkills[2] }}</span>
+								ENG: <span class="stat-text accent--text" style="font-size: 24px;">{{ pilot.mechSkills[3] }}</span>
+								]
+							</span>
+						</div>
 						<div class="row flex-container-cols">
 							<div class="col col-share">
 								<span>PILOT SKILL TRIGGER AUDIT</span>
 								<br>
-								<div class="chip-container" v-for="skill in pilot.skills" :key="skill.id">						
-									<span class="chip"><i aria-hidden="true" class="notranslate cci cci-skill"></i>{{ getSkill(skill.id) }}</span>
+								<div class="chip-container" v-for="skill in pilot.skills" :key="skill.id">
+									<span class="chip">
+										<i aria-hidden="true" class="notranslate cci cci-skill"></i>
+										{{ getSkill(skill.id) }}
+									</span>
 								</div>
 							</div>
 							<div class="col col-share">
 								<span>PILOT TALENT AUDIT</span>
 								<br>
-								<div class="chip-container" v-for="talent in pilot.talents" :key="talent.id">						
-									<span class="chip">
+								<div class="chip-container" v-for="i in 3" :key="i">
+									<span class="chip" :class="{ 'empty-chip': !pilot.talents?.[i - 1] }">
 										<i aria-hidden="true" class="notranslate cci cci-talent"></i>
-										{{ getTalent?.(talent.id)?.name || 'Unknown Talent' }} {{ 'I'.repeat(talent.rank) }}
+										{{ getTalent?.(pilot.talents?.[i - 1]?.id)?.name || 'Unknown Talent' }}
+										{{ pilot.talents?.[i - 1]?.rank ? ' ' + 'I'.repeat(pilot.talents[i - 1].rank) : '' }}
 									</span>
 								</div>
 							</div>
@@ -67,17 +77,21 @@
 							<div class="col" style="padding-top:5px">
 								<span>PROCUREMENT LICENSE AUDIT: LEVEL {{ pilot.level }}</span>
 								<br>
-								<div class="chip-container" v-for="license in pilot.licenses" :key="license.id">						
-									<span class="chip"><i aria-hidden="true" class="notranslate cci cci-license"></i>{{ getLicense(license.id, license.rank) }}</span>
+								<div class="chip-container" v-for="license in pilot.licenses" :key="license.id">
+									<span class="chip">
+										<i aria-hidden="true" class="notranslate cci cci-license"></i>
+										{{ getLicense(license.id, license.rank) }}
+									</span>
 								</div>
 							</div>
 						</div>
 						<div class="row biometrics-container">
-							<div class="biometrics"><i aria-hidden="true"
+							<div class="biometrics">
+								<i aria-hidden="true"
 									class="v-icon notranslate mdi mdi-fingerprint theme--dark grey--text text--darken-2"
-									style="font-size: 36px; margin-top:1em;"></i> BIOMETRIC RECORD VALID [[{{randomNumber(14,22)}}PB]] ::
-								OHM
-								C//{{ timeStamp(pilot.lastModified) }}</div>
+									style="font-size: 36px; margin-top:1em;"></i>
+								BIOMETRIC RECORD VALID [[{{ randomNumber(14, 22) }}PB]] :: OHM C//{{ timeStamp(pilot.lastModified) }}
+							</div>
 						</div>
 					</div>
 					<div class="col">
@@ -90,21 +104,15 @@
 				</div>
 			</div>
 			<hr role="separator" aria-orientation="horizontal" class="ma-2 v-divider theme--dark">
-			<div class="row row--dense"><span class="overline" style="line-height: 13px !important; opacity: 0.4;">
-					Improper use of this IDENT record and/or its constituent data by the record holder or any other
-					persons is punishable under the DoJ/HR A-645-c. This record is the property of the Union
-					Administrative Office and the information herein must be transmitted on request under
-					NDL-C-DISCORDANT-BREATH encryption protocols. This RM-4 record must be updated every five (5)
-					Cradle
-					Standard Years of objective time to retain GMS licensing rights. Far-field operatives that
-					anticipate deployments lasting longer than five Cradle Standard Years that have not been issued
-					a
-					man-portable Omninet Hook should apply for the RM-11-B IDENT Supplemental (b) Extension. Contact
-					your local Union Adminstrative Officer for any other matters regarding this
-					record.  V-CDL//M-265-114-831 (A) </span></div>
+			<div class="row row--dense">
+				<span class="overline" style="line-height: 13px !important; opacity: 0.4;">
+					Improper use of this IDENT record and/or its constituent data by the record holder or any other persons is punishable under the DoJ/HR A-645-c. This record is the property of the Union Administrative Office and the information herein must be transmitted on request under NDL-C-DISCORDANT-BREATH encryption protocols. This RM-4 record must be updated every five (5) Cradle Standard Years of objective time to retain GMS licensing rights. Far-field operatives that anticipate deployments lasting longer than five Cradle Standard Years that have not been issued a man-portable Omninet Hook should apply for the RM-11-B IDENT Supplemental (b) Extension. Contact your local Union Adminstrative Officer for any other matters regarding this record.  V-CDL//M-265-114-831 (A)
+				</span>
+			</div>
 		</div>
 	</div>
 </template>
+
 
 
 <script>
