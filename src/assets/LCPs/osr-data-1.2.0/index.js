c6c175152e3040e70de9632bb2cdc80fd2b62c03
jinterceptor <jensen.cocker@gmail.com>
2025-06-07 15:01:27 +0100

    Create index.js

---
+++ src/assets/LCPs/osr-data-1.2.0/index.js
@@ -0,0 +1,15 @@
+
+"use strict"
+import f from "./frames.json"
+import s from "./systems.json"
+import w from "./weapons.json"
+import t from "./talents.json"
+
+const data = {
+  frames: f,
+  systems: s,
+  weapons: w,
+  talents: t,
+}
+
+export default data;
