7b1b36e05bd17ef1bd4fc38d355998c80097d9f5
jinterceptor <jensen.cocker@gmail.com>
2025-12-11 12:31:58 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -5,30 +5,50 @@
   </div>
 
   <div id="router-view-container">
-    <router-view :members="members" />
+    <router-view
+      :animate="animate"
+      :members="members"
+    />
   </div>
+
+  <svg style="visibility: hidden; position: absolute" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+    <defs>
+      <filter id="round">
+        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5" result="goo" />
+        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+      </filter>
+    </defs>
+  </svg>
+
+  <audio autoplay>
+    <source src="/startup.ogg" type="audio/ogg" />
+  </audio>
 </template>
 
 <script>
 import Header from "./components/layout/Header.vue";
 import Sidebar from "./components/layout/Sidebar.vue";
-import Papa from "papaparse";
 import Config from "@/assets/info/general-config.json";
+import Papa from "papaparse";
 
 export default {
-  components: { Header, Sidebar },
+  components: {
+    Header,
+    Sidebar,
+  },
 
   data() {
     return {
       animate: Config.animate,
       planetPath: Config.planetPath,
       header: Config.header,
-      members: [], // this will hold all member names
+      members: [], // will store member names
     };
   },
 
   created() {
-    this.setTitleFavicon(Config.defaultTitle + " MEMBERS", Config.icon);
+    this.setTitleFavicon(Config.defaultTitle + " MISSION BRIEFING", Config.icon);
 
     // Load MembersMaster CSV
     this.loadMembersCSV(
@@ -36,16 +56,12 @@ export default {
     );
   },
 
-  mounted() {
-    this.$router.push("/pilots"); // we can rename later
-  },
-
   methods: {
     setTitleFavicon(title, favicon) {
       document.title = title;
       const link = document.createElement("link");
-      link.rel = "shortcut icon";
-      link.href = favicon;
+      link.setAttribute("rel", "shortcut icon");
+      link.setAttribute("href", favicon);
       document.head.appendChild(link);
     },
 
@@ -55,12 +71,9 @@ export default {
         header: true,
         skipEmptyLines: true,
         complete: (results) => {
-          console.log("MembersMaster headers:", results.meta.fields);
-
-          // Column 2 = 'Name' (assuming first real data row)
-          this.members = results.data
-            .map((row) => row["Name"]?.trim())
-            .filter((name) => name); // remove empty
+          // MembersMaster: the column we want is 'Name'
+          const names = results.data.map((row) => row["Name"]).filter((n) => n);
+          this.members = names;
           console.log("Members loaded:", this.members.length);
         },
         error: (err) => {
