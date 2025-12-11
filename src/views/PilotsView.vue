61583942619df1a97278b56e540d363c9bb89492
jinterceptor <jensen.cocker@gmail.com>
2025-12-11 12:32:41 +0000

    Update PilotsView.vue

--- src/views/PilotsView.vue
+++ src/views/PilotsView.vue
@@ -1,15 +1,29 @@
 <template>
-  <section class="section-container">
-    <h1>Unit Members</h1>
-    <ul v-if="members.length">
-      <li v-for="name in members" :key="name">{{ name }}</li>
-    </ul>
-    <p v-else>Loading members...</p>
+  <section id="members" class="section-container">
+    <div style="height: 52px; overflow: hidden">
+      <div class="section-header clipped-medium-backward">
+        <img src="/icons/license.svg" alt="Members Icon" />
+        <h1>Unit Members</h1>
+      </div>
+      <div class="rhombus-back">&nbsp;</div>
+    </div>
+
+    <div class="section-content-container">
+      <div v-if="members.length === 0">
+        Loading members...
+      </div>
+      <ul v-else class="member-list">
+        <li v-for="(member, index) in members" :key="index">
+          {{ member }}
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
+  name: "PilotsView",
   props: {
     members: {
       type: Array,
@@ -22,5 +36,26 @@ export default {
 <style scoped>
 .section-container {
   padding: 1rem;
+}
+
+.section-header {
+  display: flex;
+  align-items: center;
+}
+
+.section-header img {
+  width: 32px;
+  margin-right: 1rem;
+}
+
+.member-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.member-list li {
+  padding: 0.25rem 0;
+  border-bottom: 1px solid #ccc;
 }
 </style>
