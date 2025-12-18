66939ec2db61b2a2cc8ee5e4fa2ee4ac53cc0438
jinterceptor <jensen.cocker@gmail.com>
2025-12-18 18:25:42 +0000

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -81,7 +81,7 @@ export default {
     planetPath: { type: String, required: true },
     header: { type: Object, required: true },
     /** Horizontal offset (px) for the auth pill; increase to shift right */
-    authOffsetX: { type: Number, default: 325 },
+    authOffsetX: { type: Number, default: 315 },
     /** Vertical offset (px) for the auth pill */
     authOffsetY: { type: Number, default: 10 },
   },
