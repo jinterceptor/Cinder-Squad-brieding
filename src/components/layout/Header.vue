df12e3d0906504ce0e97cbefd1703ec74c1e5b88
jinterceptor <jensen.cocker@gmail.com>
2025-12-18 14:46:22 +0000

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -142,7 +142,7 @@ export default {
 .auth-status{
   --auth-left: 750px;
   position: absolute;
-  top: 0px;                /* slightly higher */
+  top: 10px;                /* slightly higher */
   left: var(--auth-left);
   display: grid;
   gap: .4rem;
